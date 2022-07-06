038e55e2026150a3d426fa1ca6c49a4ca7eb08a2
monikkacha <monikkacha@gmail.com>
2022-07-06 10:57:43 +0530

    dio cache added

--- lib/api_store.dart
+++ lib/api_store.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:dio_http_cache/dio_http_cache.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_provider/todo.dart';
-import 'package:http/http.dart';
-import 'package:http/http.dart' as http;
 
 class ApiStore extends ChangeNotifier {
   List<TodoResponse> list = [];
@@ -16,13 +19,28 @@ class ApiStore extends ChangeNotifier {
 
   bool get loadingMoreData => isLoadingMoreData;
 
+  String baseUrl = "https://jsonplaceholder.typicode.com";
+
+  Dio dio = Dio();
+
+  ApiStore() {
+    init();
+  }
+
+  init() {
+    dio.interceptors
+        .add(DioCacheManager(CacheConfig(baseUrl: baseUrl)).interceptor);
+  }
+
   Future<void> fetchData() async {
     isLoading = true;
     notifyListeners();
-    Response response = await http.get(Uri.parse(
-        "https://jsonplaceholder.typicode.com/todos?_limit=$limit&_page=$page"));
+    Response response = await dio.get(
+        "$baseUrl/todos?_limit=$limit&_page=$page",
+        options: buildCacheOptions(Duration(days: 7)));
     if (response.statusCode == 200 || response.statusCode == 201) {
-      list = categoryResponseFromJson(response.body);
+      String body = jsonEncode(response.data);
+      list = categoryResponseFromJson(body);
       page++;
     }
     isLoading = false;
@@ -33,11 +51,15 @@ class ApiStore extends ChangeNotifier {
     if (isLoadingMoreData) return;
     isLoadingMoreData = true;
     notifyListeners();
-    Response response = await http.get(Uri.parse(
-        "https://jsonplaceholder.typicode.com/todos?_limit=$limit&_page=$page"));
+
+    Response response = await dio.get(
+        "$baseUrl/todos?_limit=$limit&_page=$page",
+        options: buildCacheOptions(Duration(days: 7)));
+
     if (response.statusCode == 200 || response.statusCode == 201) {
       var tempList = List.from(list);
-      tempList.addAll(categoryResponseFromJson(response.body));
+      String body = jsonEncode(response.data);
+      tempList.addAll(categoryResponseFromJson(body));
       list = List.from(tempList);
       page++;
     }
